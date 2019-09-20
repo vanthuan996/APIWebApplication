6dccd57f4c017d590046c3f54f7cb0e5c7fc4695
Thuan Van <thuan.dang@nois.vn>
2019-09-20 09:58:36 +0700

    first commit

---
+++ APIWebApplication/Models/ApiWebContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace APIWebApplication.Models
+{
+    public partial class ApiWebContext : DbContext
+    {
+        public ApiWebContext()
+        {
+        }
+
+        public ApiWebContext(DbContextOptions<ApiWebContext> options)
+            : base(options)
+        {
+        }
+
+        // Unable to generate entity type for table 'dbo.User'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-I0CR0MQ;Initial Catalog=ApiWeb;Integrated Security=True;Persist Security Info=False;MultipleActiveResultSets = true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+        }
+    }
+}
---
+++ APIWebApplication/Startup.cs
@@ -0,0 +1,71 @@
+﻿using APIWebApplication.Middlewares;
+using APIWebApplication.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ASPNETCoreSwaggerDemo
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+
+            Environment = env;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "My API",
+                    Description = "My First ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "ApiWebApp", Email = "dangvanthuan0104@gmail.com", Url = "www.vanthuan.com" }
+                });
+            });
+            services.AddDbContext<ApiWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+        }
+
+
+    }
+}
